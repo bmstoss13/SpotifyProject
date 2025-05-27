f8ecc5c950951e366c66cb15f1375b865d394de4
imjel <maxineendieveri@gmail.com>
2025-05-27 12:54:10 -0700

    profile page styling

--- Spotify-Project/frontend/src/pages/Profile.jsx
+++ Spotify-Project/frontend/src/pages/Profile.jsx
@@ -2,28 +2,32 @@ import { useEffect, useState } from 'react'
 import { getProfile } from '../services/api'
 import Sidebar from '../components/Sidebar'
 import ProfileEditModal from '../components/ProfileEditModal'
+import { CiUser } from "react-icons/ci";
+import SquareContainer from '../components/SquareContainer'
+import { FaRegEdit } from "react-icons/fa";
 
 // features:
-	// user can edit profile
-	// user can choose to display artists/songs from their top songs and liked songs pages
-	// can make their profile private (hides from discover page)
+// user can edit profile
+// user can choose to display artists/songs from their top songs and liked songs pages
+// can make their profile private (hides from discover page)
 
 function Profile() {
-    const[profile, setProfile] = useState(null)
-    const[loading, isLoading] = useState(true)
+
+	const [profile, setProfile] = useState(null)
+	const [loading, isLoading] = useState(true)
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        getProfile()
-            .then((res) => {
-                setProfile(res.data)
-                isLoading(false)
-            })
-            .catch((e) => {
-                console.error("Error getting profile:", e);
-                isLoading(false)
-            });
-    }, [])
+	useEffect(() => {
+		getProfile()
+			.then((res) => {
+				setProfile(res.data)
+				isLoading(false)
+			})
+			.catch((e) => {
+				console.error("Error getting profile:", e);
+				isLoading(false)
+			});
+	}, [])
 
 	const handleProfileUpdate = (updatedData) => {
 		// spotify api stuff goes here
@@ -31,25 +35,52 @@ function Profile() {
 		setIsModalOpen(false)
 	};
 
-    if (loading) return <p>Loading...</p>
-    if (!profile) return <p>Profile not found.</p>
+	if (loading) return <p>Loading...</p>
+	if (!profile) return <p>Profile not found.</p>
 
-    return(
-        <div>
-			<Sidebar/>
-			<h3>Profile</h3>
-            <h1>{profile.display_name}</h1>
-            <p>{profile.user_bio}</p>
-			<h2>Top Artists</h2>
+	return (
+		<div className='profile-container'>
+			<Sidebar />
+			<title>User Profile</title>
+			<header className='profile-header'>
+				{profile.image ? (
+					<img
+						className='profile-picture'
+						src={profile.image}
+						alt='Profile picture'
+					/>
+				) : (
+					<CiUser className='profile-picture fallback-icon' />
+				)}
+				<div className='profile-overview'>
+					{/* <h3>Profile</h3> */}
+
+					{profile.display_name ? (
+						<h1>{profile.display_name}</h1>
+					) : (
+						<h1>Display Name</h1>
+					)}
+
+					<p>{profile.user_bio}</p>
+
+					{/* !! follower info goes here !! */}
+					<div className='follower-edit'>
+						<p>Followers: </p>
+						<p>Following: </p>
+						<button
+							className="edit-profile-btn"
+							onClick={() => setIsModalOpen(true)}>
+							<FaRegEdit/>
+						</button>
+					</div>
+				</div>
+			</header>
+			<div className='profile-body'>
+				<h2>Top Artists</h2>
 				<p>artists go here</p>
-			<h2>Top Songs</h2>
-				<p>songs go here</p>
-			<button
-				className="edit-profile-btn"
-				onClick={() => setIsModalOpen(true)}>
-				Edit
-			</button>
-
+				<h2>Top Songs</h2>
+				<SquareContainer />
+			</div>
 			{isModalOpen && (
 				<ProfileEditModal
 					profileData={profile}
@@ -57,7 +88,7 @@ function Profile() {
 					onSubmit={handleProfileUpdate}
 				/>
 			)}
-        </div>
-    )
+		</div>
+	)
 };
 export default Profile;
--- Spotify-Project/frontend/src/services/api.js
+++ Spotify-Project/frontend/src/services/api.js
@@ -4,5 +4,9 @@ const port = 3000;
 
 const request = axios.create({ baseURL: `https://test-spotify-site.local:${port}` })
 
-export const getProfile = () => request.get('/profile')
+// export const getProfile = () => request.get('/profile')
+export const getProfile = () => {
+  return axios.get('https://test-spotify-site.local:3000/profile');
+};
+
 export const getDiscover = () => request.get('/discover')
