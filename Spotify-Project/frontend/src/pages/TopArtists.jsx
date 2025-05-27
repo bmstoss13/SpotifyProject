2e7392f01a2904b5bc2bc68c8c108e241e198a82
SarathyS101 <se.sarathy17@gmail.com>
2025-05-27 12:21:21 -0400

    basic page

--- Spotify-Project/frontend/src/pages/TopArtists.jsx
+++ Spotify-Project/frontend/src/pages/TopArtists.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import Artist from '../components/Artist';
+import React from "react";
+import Artist from "../components/Artist";
+import SquareContainer from "../components/SquareContainer";
 const TopArtists = () => {
+  const artists = [
+    { artist: "Drake", pfp: "https://example.com/drake.jpg" },
+    { artist: "Taylor Swift", pfp: "https://example.com/taylor.jpg" },
+    { artist: "The Weeknd", pfp: "https://example.com/weeknd.jpg" },
+    { artist: "Ed Sheeran", pfp: "https://example.com/ed.jpg" },
+    { artist: "Billie Eilish", pfp: "https://example.com/billie.jpg" },
+  ];
   return (
     <div>
-     <Artist />
+      <SquareContainer type={"artists"} top={artists} />
     </div>
   );
 };
--- Spotify-Project/frontend/src/pages/TopSongs.jsx
+++ Spotify-Project/frontend/src/pages/TopSongs.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Song from '../components/Song'; 
 import SquareContainer from '../components/SquareContainer';
 const TopSongs = () => {
+  const songs =[
+    { title: "Song 1", artist: "Artist A" },
+    { title: "Song 2", artist: "Artist B" },
+    { title: "Song 3", artist: "Artist C" },
+    { title: "Song 4", artist: "Artist D" },
+    { title: "Song 5", artist: "Artist E" }
+  ]
   return (
     <div>
-     <SquareContainer />
-     {/* <Song /> */}
+     <SquareContainer type={"songs"} top={songs}/>
     </div>
   );
 };
