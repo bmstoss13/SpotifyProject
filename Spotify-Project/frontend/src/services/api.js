c85ba906bb4792acd96310a7ded2b98c5288b8f3
aayushmap34 <hza9yx@virginia.edu>
2025-05-27 10:57:27 -0400

    frontend API call

--- Spotify-Project/frontend/src/services/api.js
+++ Spotify-Project/frontend/src/services/api.js
@@ -3,3 +3,13 @@ import axios from 'axios'
 const request = axios.create({ baseURL: 'http://localhost:3000/profile/' })
 
 export const getProfile = () => request.get('/')
+
+export async function fetchLikedSongs(token) {
+    const res = await axios.get('http://localhost:5000/api/liked-songs', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return res.data;
+  }
+  
