86651444fbb05d03a7a6d390ae68aa87cb7ae820
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-27 11:51:54 -0400

    fix: user list (/discover page returns arbitrary object created in backend)

--- Spotify-Project/frontend/src/pages/Discover.jsx
+++ Spotify-Project/frontend/src/pages/Discover.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from 'react'
+import { getDiscover } from '../services/api'
 import React from "react";
 
 const Discover = () => {
+  const[users, setUsers] = useState([]);
+  const[loading, isLoading] = useState(true);
+
+  useEffect(() => {
+    getDiscover()
+      .then((res) => {
+        if(Array.isArray(res.data)){
+          setUsers(res.data);
+        }
+        else if (res.data && Array.isArray(res.data.users)){
+          setUsers(res.data.users);
+        }
+        else{
+          console.error("Received data is not an array or does not contain an array");
+          setUsers([]);
+        }
+
+        isLoading(false)
+      })
+      .catch((e) => {
+        console.error("Error getting other user profiles:", e);
+        isLoading(false)
+      });
+    }, [])
+
+    if (loading) return <p>Loading users...</p>
+    if (users.length === 0) return <p>Users not found.</p>
+
   return (
     <div>
       <h1>Discover Page</h1>
-      <p>Content for discovering new music, artists, etc.</p>
+      <ul>{users.map((user) => (
+        <li key={user.id}>
+          <span>{user.username}</span>
+        </li>
+      ))}</ul>
     </div>
   );
 };
