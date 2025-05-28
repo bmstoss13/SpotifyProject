931f77e4d6fa478940315d86f75b08ab08bdf96d
imjel <maxineendieveri@gmail.com>
2025-05-28 08:13:35 -0700

    integrating main w/ profile

--- Spotify-Project/frontend/src/pages/Profile.jsx
+++ Spotify-Project/frontend/src/pages/Profile.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from 'react'
 import { getProfile } from '../services/api'
-<<<<<<< HEAD
 import Sidebar from '../components/Sidebar'
 import ProfileEditModal from '../components/ProfileEditModal'
 import { CiUser } from "react-icons/ci";
 import SquareContainer from '../components/SquareContainer'
 import { FaRegEdit } from "react-icons/fa";
+import { useAuth } from '../components/AuthContext';
 
 // features:
 // user can edit profile
 // user can choose to display artists/songs from their top songs and liked songs pages
 // can make their profile private (hides from discover page)
-=======
-import { useAuth } from '../components/AuthContext';
->>>>>>> ccb3a02eacc7b58442907e21d7383f502c4bc616
 
 function Profile() {
 
@@ -21,8 +18,7 @@ function Profile() {
 	const [loading, isLoading] = useState(true)
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
-<<<<<<< HEAD
-	useEffect(() => {
+		useEffect(() => {
 		getProfile()
 			.then((res) => {
 				setProfile(res.data)
@@ -33,6 +29,30 @@ function Profile() {
 				isLoading(false)
 			});
 	}, [])
+
+	const { accessToken, setAccessToken, setRefreshToken, setExpiresIn } = useAuth();
+	useEffect(() => {
+		const hashParams = new URLSearchParams(window.location.hash.slice(1));
+		console.log(hashParams);
+		const auth = hashParams.get("access_token");
+		const refresh = hashParams.get("refresh_token");
+		const expires = hashParams.get("expires_in");
+		console.log("auth:" + auth);
+		if (auth) {
+			setAccessToken(auth);
+			setRefreshToken(refresh);
+			setExpiresIn(expires);
+
+			// window.location.replace("/profile");
+		}
+
+		else {
+			window.location.replace("/");
+		}
+
+		// Clean up the paramerers from the URL
+		// window.history.replaceState({}, document.title, window.location.pathname);
+	}, []);
 
 	const handleProfileUpdate = (updatedData) => {
 		// spotify api stuff goes here
@@ -78,17 +98,17 @@ function Profile() {
 			</header>
 			<div className='profile-body'>
 				{/* {profile.showTopArtists && ( */}
-					<section>
-						<h2>Top Artists</h2>
-						<p>artists go here</p>
-					</section>
+				<section>
+					<h2>Top Artists</h2>
+					<p>artists go here</p>
+				</section>
 				{/* )} */}
 
 				{/* {profile.displayTopSongs && ( */}
-					<section>
-						<h2>Top Songs</h2>
-						<SquareContainer />
-					</section>
+				<section>
+					<h2>Top Songs</h2>
+					<SquareContainer />
+				</section>
 				{/* )} */}
 
 			</div>
@@ -101,41 +121,6 @@ function Profile() {
 			)}
 		</div>
 	)
-=======
-    const { accessToken, setAccessToken, setRefreshToken, setExpiresIn } = useAuth();
-    useEffect(() => {
-        const hashParams = new URLSearchParams(window.location.hash.slice(1)); 
-        console.log(hashParams);
-        const auth = hashParams.get("access_token");
-        const refresh = hashParams.get("refresh_token");
-        const expires = hashParams.get("expires_in");
-        console.log("auth:" + auth);
-        if (auth) {
-            setAccessToken(auth);
-            setRefreshToken(refresh);
-            setExpiresIn(expires);
-          
-            // window.location.replace("/profile");
-        }
-
-        else{
-            window.location.replace("/");
-        }
-    
-            // Clean up the paramerers from the URL
-        // window.history.replaceState({}, document.title, window.location.pathname);
-    }, []);
-
-    if (loading) return <p>Loading...</p>
-    if (!profile) return <p>Profile not found.</p>
-
-    return(
-        <div>
-            <h1>{profile.name}</h1>
-            <p>{profile.bio}</p>
-            {/* <p>{accessToken}</p> */}
-        </div>
-    )
->>>>>>> ccb3a02eacc7b58442907e21d7383f502c4bc616
 };
+
 export default Profile;
