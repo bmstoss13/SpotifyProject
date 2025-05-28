27fb052d66ad870d4eb668ab7b5f8dcfdfdbc785
aayushmap34 <hza9yx@virginia.edu>
2025-05-28 16:26:05 -0400

    protected route

---
+++ Spotify-Project/frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+export default function ProtectedRoute({ children }) {
+  const { accessToken, loading } = useAuth();
+  if (loading) return <p>Loading...</p>;
+  const { accessToken } = useAuth();
+  if (!accessToken) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
