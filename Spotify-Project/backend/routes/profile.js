8ea3984ec9ba2a36037badd20b91dc58105d57c6
imjel <maxineendieveri@gmail.com>
2025-05-28 09:07:49 -0700

    testing spotify api stuff

--- Spotify-Project/backend/routes/profile.js
+++ Spotify-Project/backend/routes/profile.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/:userId', async (req, res) => {
+	// const {userId} = req.params;
+	// const docRef = doc(db, "users", userId);
+    // const docSnap = await getDoc(docRef);
+
+    // if (!docSnap.exists) {
+    //   return res.status(404).json({ error: "Profile not found" });
+    // }
+	// res.json(docSnap.data());
   res.json({ name: 'Obama', bio: 'I love coding.' });
 });
 
