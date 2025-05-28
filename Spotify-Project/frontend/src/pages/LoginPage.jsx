543a6e88572e073029e7f73c9b1d69556bbe4234
SarathyS101 <se.sarathy17@gmail.com>
2025-05-28 13:50:45 -0400

    fix?

--- Spotify-Project/frontend/src/pages/LoginPage.jsx
+++ Spotify-Project/frontend/src/pages/LoginPage.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect } from 'react';
 import { initiateSpotifyLogin } from '../services/api';
 import { useAuth } from '../components/AuthContext';
-
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
-    const handleLogin = () => {
-        initiateSpotifyLogin(); 
-    };
+  const { accessToken } = useAuth();
+  const navigate = useNavigate();
 
-    // const { setAccessToken, setRefreshToken, setExpiresIn } = useAuth();
+  useEffect(() => {
+    if (accessToken) {
+      navigate('/profile'); // redirect if already logged in
+    }
+  }, [accessToken, navigate]);
 
-    
+  const handleLogin = () => {
+    initiateSpotifyLogin();
+  };
 
-
-
-    return (
-        <div>
-            <h1>Welcome to spoti.love</h1>
-            <button onClick={handleLogin}>Login with Spotify</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to spoti.love</h1>
+      <button onClick={handleLogin}>Login with Spotify</button>
+    </div>
+  );
 }
 
 export default LoginPage;
