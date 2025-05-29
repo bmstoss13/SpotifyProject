21b30eb3cddf53e98d845547880047be47aaa4bf
imjel <maxineendieveri@gmail.com>
2025-05-29 12:43:38 -0700

    login page styling

--- Spotify-Project/frontend/src/pages/LoginPage.jsx
+++ Spotify-Project/frontend/src/pages/LoginPage.jsx
@@ -2,27 +2,30 @@ import React, { useEffect } from 'react';
 import { initiateSpotifyLogin } from '../services/api';
 import { useAuth } from '../components/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import '../styles/LoginPage.css'
 
 function LoginPage() {
-  const { accessToken } = useAuth();
-  const navigate = useNavigate();
+	const { accessToken } = useAuth();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (accessToken) {
-      navigate('/profile'); // redirect if already logged in
-    }
-  }, [accessToken, navigate]);
+	useEffect(() => {
+		if (accessToken) {
+			navigate('/profile'); // redirect if already logged in
+		}
+	}, [accessToken, navigate]);
 
-  const handleLogin = () => {
-    initiateSpotifyLogin();
-  };
+	const handleLogin = () => {
+		initiateSpotifyLogin();
+	};
 
-  return (
-    <div>
-      <h1>Welcome to Heartify</h1>
-      <button onClick={handleLogin}>Login with Spotify</button>
-    </div>
-  );
+	return (
+		<div className='login-page full-screen'>
+			<div className='login-container'>
+				<h1 className='welcome-text'>Welcome to Heartify</h1>
+				<button onClick={handleLogin}>Login with Spotify</button>
+			</div>
+		</div>
+	);
 }
 
 export default LoginPage;
