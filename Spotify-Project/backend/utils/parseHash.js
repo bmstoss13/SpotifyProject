c6b9a2aa95e2cf5c55ce758e987e1b515546287f
aayushmap34 <hza9yx@virginia.edu>
2025-05-29 11:20:59 -0400

    add logic to connect to spotify API

---
+++ Spotify-Project/backend/utils/parseHash.js
@@ -0,0 +1,8 @@
+export function parseHash() {
+    if (!window.location.hash) return {};
+    return window.location.hash.substring(1).split('&').reduce((acc, pair) => {
+      const [key, value] = pair.split('=');
+      acc[key] = decodeURIComponent(value);
+      return acc;
+    }, {});
+  }
