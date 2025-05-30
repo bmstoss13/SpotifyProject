a983587ae269caa4a1097321282d2c3d3f03c7d3
aayushmap34 <hza9yx@virginia.edu>
2025-05-30 11:00:52 -0400

    delete Chakra Provider

--- Spotify-Project/frontend/src/main.jsx
+++ Spotify-Project/frontend/src/main.jsx
@@ -4,16 +4,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './components/AuthContext';
 import './index.css'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-    </ChakraProvider>
   </React.StrictMode>
 );
